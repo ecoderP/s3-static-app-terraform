167e6b7d33c62d9671269ae36c5c7ba8f0b8d663
ecoderP <paul.onyebuchi20@gmail.com>
2026-05-23 11:43:26 -0700

    Fixed staging env

--- terraform/environments/staging/outputs.tf
+++ terraform/environments/staging/outputs.tf
@@ -5,3 +5,11 @@ output "distribution_domain_name" {
 output "bucket_name" {
   value = module.s3_static_site.bucket_name
 }
+
+output "github_actions_role_arn" {
+  value = module.github_oidc.github_actions_role_arn
+}
+
+output "cloudfront_distribution_id" {
+  value = module.cloudfront.distribution_id
+}
